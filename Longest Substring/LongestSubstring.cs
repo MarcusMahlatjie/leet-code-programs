282d1ed898a0c7b2e300687880ca21ee915cd4b5
Marcus Mahlatjie <nicmahlatjie@icloud.com>
2023-12-26 10:11:52 +0200

    add another challenge

---
+++ Longest Substring/LongestSubstring.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leet_code_programs
+{
+    internal class LongestSubstring
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            int maxLength = 0;
+            StringBuilder subStr = new StringBuilder();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                for (int j = i; j < s.Length; j++)
+                {
+                    if (subStr.Length > 0)
+                    {
+                        if (!subStr.ToString().Contains(s[j]))
+                        {
+                            subStr.Append(s[j]);
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        subStr.Append(s[j]);
+                    }
+                }
+
+                if (subStr.Length > maxLength)
+                    maxLength = subStr.Length;
+
+                subStr = new StringBuilder();
+            }
+
+            return maxLength;
+
+        }
+    }
+}
